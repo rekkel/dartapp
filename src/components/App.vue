5a361a79ef863f84904e23921d883b1ee2a95792
Rick Ekkelboom <rick.ekkelboom@gmail.com>
2017-12-26 15:55:05 +0100

    Added Vuex, VueRouter and bootstrap-vue packages

---
+++ src/components/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <NavHeader></NavHeader>
+  </div>
+</template>
+
+<script>
+import NavHeader from './NavHeader.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    NavHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin:0;
+  padding:0;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+  <b-navbar toggleable="md" type="dark" variant="info">
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="#">Darts App</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item href="#" disabled>History</b-nav-item>
+
+        <b-nav-item-dropdown text="501" right>
+          <b-dropdown-item href="#">1 Speler</b-dropdown-item>
+          <b-dropdown-item href="#">2 Spelers</b-dropdown-item>
+          <b-dropdown-item href="#">3 Spelers</b-dropdown-item>
+          <b-dropdown-item href="#">4 Spelers</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="Tactics" right>
+          <b-dropdown-item href="#">1 Speler</b-dropdown-item>
+          <b-dropdown-item href="#">2 Spelers</b-dropdown-item>
+          <b-dropdown-item href="#">3 Spelers</b-dropdown-item>
+          <b-dropdown-item href="#">4 Spelers</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+    </b-collapse>
+</b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
