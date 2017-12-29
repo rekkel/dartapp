69cde070795b2bb528c453fccc5abff67e80cce5
Rick Ekkelboom <rick.ekkelboom@gmail.com>
2017-12-29 23:11:50 +0100

    Fixed mobile navigation

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -8,28 +8,28 @@
 
     <b-collapse is-nav id="nav_collapse">
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
 
-        <!-- <b-nav-item href="#" disabled>History</b-nav-item> -->
+          <b-nav-item href="#" disabled>History</b-nav-item>
 
-        <!-- <b-nav-item-dropdown text="501" right>
-          <b-dropdown-item href="#">1 Speler</b-dropdown-item>
-          <b-dropdown-item :to="{ path: '/vijf2' }">2 Spelers</b-dropdown-item>
-          <b-dropdown-item href="#">3 Spelers</b-dropdown-item>
-          <b-dropdown-item href="#">4 Spelers</b-dropdown-item>
-        </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="501" right>
+            <b-dropdown-item href="#">1 Speler</b-dropdown-item>
+            <b-dropdown-item :to="{ path: '/vijf2' }">2 Spelers</b-dropdown-item>
+            <b-dropdown-item href="#">3 Spelers</b-dropdown-item>
+            <b-dropdown-item href="#">4 Spelers</b-dropdown-item>
+          </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown text="Tactics" right>
-          <b-dropdown-item href="#">1 Speler</b-dropdown-item>
-          <b-dropdown-item href="#">2 Spelers</b-dropdown-item>
-          <b-dropdown-item href="#">3 Spelers</b-dropdown-item>
-          <b-dropdown-item href="#">4 Spelers</b-dropdown-item>
-        </b-nav-item-dropdown> -->
-      </b-navbar-nav>
+          <b-nav-item-dropdown text="Tactics" right>
+            <b-dropdown-item href="#">1 Speler</b-dropdown-item>
+            <b-dropdown-item href="#">2 Spelers</b-dropdown-item>
+            <b-dropdown-item href="#">3 Spelers</b-dropdown-item>
+            <b-dropdown-item href="#">4 Spelers</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
 
-    </b-collapse>
-</b-navbar>
+      </b-collapse>
+  </b-navbar>
   </div>
 </template>
 
@@ -44,13 +44,4 @@ export default {
 </script>
 
 <style>
-.navbar {
-  height:4vh;
-  z-index: 999;
-}
-
-.navbar-collapse {
-  z-index: 9999;
-  opacity: 1;
-}
 </style>
